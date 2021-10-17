098b3260089cd6d940cadb40b5831ec5e366a96b
Hasan-Quraishi <80095625+Hasan-Quraishi@users.noreply.github.com>
2021-10-18 09:36:22 +1100

    Final Files

---
+++ map.js
@@ -0,0 +1,9 @@
+var vg_1 = "map.vega";
+var vg_2="heat.vega";
+
+vegaEmbed("#viz", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#viz1", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
